b2a2b846f85509f56ad4e17b5f494d84c8df8f03
jhohansilva <jhohanf.silva@gmail.com>
2019-06-13 23:02:01 -0500

    Cambios visuales

--- pasarArchivosApp.bat
+++ pasarArchivosApp.bat
@@ -3,10 +3,10 @@
 
 IF ERRORLEVEL 2 GOTO ERROR
 
-xcopy C:\xampp\htdocs\devApp "C:\Users\Jhohan\appOnsen\www" /E /I /Y /exclude:exclusion.txt
+xcopy C:\xampp\htdocs\titanApp "C:\Users\JhohanSilva\appNativa\www" /E /I /Y /exclude:exclusion.txt
 echo Proceso Completado
 
-cd "C:\Users\Jhohan\appOnsen"
+cd "C:\Users\JhohanSilva\appNativa"
 cordova run android
 
 pause
